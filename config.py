23ec4a4504f097293bb3ec718f2077390cbcc0a6
artemne555 <84119127+artemne555@users.noreply.github.com>
2024-05-15 16:03:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 HOME_DIR = '/home/student/AI_bot'
 LOGS = f'{HOME_DIR}/logs.txt'
-
+DB_FILE = f'{HOME_DIR}/ЭТО_БАЗА.db'
 IAM_TOKEN_PATH = f'{HOME_DIR}/creds/iam_token.txt'
 FOLDER_ID_PATH = f'{HOME_DIR}/creds/folder_id.txt'
 BOT_TOKEN_PATH = f'{HOME_DIR}/creds/bot_token.txt'
